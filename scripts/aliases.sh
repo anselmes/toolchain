07330bec04a1372bb7a3f952cf4b9d99f4c2760a
Schubert Anselme <schubert@anselm.es>
2024-10-29 22:26:37 -0400

    chore: Add configure script (#3)
    This commit adds a configure script to the repository. The configure script handles the installation of necessary dependencies and sets up the environment. This commit ensures that the project has all the required dependencies and a proper environment for development.
    
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,11 +4,16 @@
 alias dir="dir --color=auto"
 alias vdir="vdir --color=auto"
 
-alias ls="ls -F --color=auto --show-control-chars"
+alias ls="ls -G"
 alias ll="ls -l"
-alias la="ls -A"
-alias l="ls -CF"
+alias la="ls -lAFh"
+alias l="ls -lCF"
 
 alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
+
+alias vbmc="sudo -E vbmc"
+alias virsh="sudo -E virsh"
+alias virt-install="sudo -E virt-install"
+alias virt-manager="sudo -E virt-manager"
--- scripts/environment.sh
+++ scripts/environment.sh
@@ -12,15 +12,41 @@ export GO11MODULE="on"
 export SCRIPTS="/workspace/scripts"
 export TOOLS="/workspace/tools"
 
+export LOCAL_BIN="${HOME}/.local"
+
 export CARGO_HOME="/usr/local/rust/cargo"
 export GOPATH="/usr/local/go"
 export KREW_ROOT="/usr/local/krew"
 export RUSTUP_HOME="/usr/local/rust/rustup"
 
-export PATH="${KREW_ROOT}/bin:${CARGO_HOME}/bin:${GOPATH}:${TOOLS}${PATH:+:${PATH}}"
+export PATH="${LOCAL_BIN}:${KREW_ROOT}/bin:${CARGO_HOME}/bin:${GOPATH}:${TOOLS}${PATH:+:${PATH}}"
 
 # SSH Agent
-eval "$(ssh-agent -s)"
-ssh-add -l >>/dev/null
-exit_code=$?
-[[ ! ${exit_code} -eq 0 ]] && ssh-add -k
+if ! ssh-add -l >>/dev/null; then
+  eval "$(ssh-agent -s)"
+  ssh-add -k
+fi
+
+# Functions
+cache() {
+  CACHE_DIR=".cache"
+
+  REPO="${1}"
+  ITEMS="${2}"
+  VERSION="${3:-main}"
+
+  [[ -z "${REPO}" ]] && echo "ERROR: repository is required" && return 1
+  [[ -z "${ITEMS}" ]] && echo "ERROR: item list is required" && return 1
+
+  if [[ ! -d "${CACHE_DIR}" ]]; then
+    mkdir -p "${CACHE_DIR}"
+    tmp="$(mktemp -d)"
+    gh repo clone "${REPO}" "${tmp}" -- --depth 1 --branch "${VERSION}"
+
+    for ITEM in ${ITEMS[@]}; do
+      cp -r "${tmp}/${ITEM}" "${CACHE_DIR}/"
+    done
+
+    rm -rf "${tmp}"
+  fi
+}
