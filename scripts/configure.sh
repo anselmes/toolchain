2706d073a303d4e841bf63fb53dd6de351fd0887
Schubert Anselme <schubert@anselm.es>
2024-11-03 12:53:54 -0500

    devcontainer: Update devcontainer config
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-3.0
 
-DIR="$(dirname $(realpath $(dirname "${0}")))"
-source "${DIR}/scripts/aliases.sh"
-source "${DIR}/scripts/environment.sh"
+source scripts/aliases.sh
+source scripts/environment.sh
 
 # dependencies
 if [[ -n $(command -v "apt-get") ]]; then
@@ -11,12 +10,13 @@ if [[ -n $(command -v "apt-get") ]]; then
     "curl"
     "git"
     "gnupg2"
+    "shfmt"
     "zsh"
   )
 
-  sudo apt-get update -y
   for command in "${commands[@]}"; do
-    if [[ -z $(command -v "${command}") ]]; then
+    if ! grep -qa "${command}" <(apt list --installed); then
+      sudo apt-get update -yq
       sudo apt-get install -y "${command}"
     fi
   done
@@ -31,9 +31,18 @@ if [[ -n $(command -v "usermod") ]]; then
     "sudo"
   )
 
+  # note: always set the user to "devcontainer" if it exists
+  if [[ "devcontainer" == "$(whoami)" ]]; then
+    USER="devcontainer"
+  else
+    USER="$(whoami)"
+  fi
+
   for g in "${groups[@]}"; do
-    sudo usermod -aG "${g}" "${USER}" || true
+    sudo usermod -aG "${g}" "${USER}"
   done
+
+  id "${USER}"
 fi
 
 # environment
@@ -44,30 +53,31 @@ if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
 fi
 
 ln -sf \
-  "${DIR}/modules/dotfiles/.bashrc" \
-  "${DIR}/modules/dotfiles/.zshrc" \
-  "${DIR}/modules/dotfiles/.editorconfig" \
-  "${DIR}/modules/dotfiles/.commitlintrc" \
-  "${DIR}/modules/dotfiles/.idea" \
-  "${DIR}/modules/dotfiles/.vscode" \
-  "${DIR}/.trunk/configs/."* \
-  "${DIR}"
+  modules/dotfiles/.bashrc \
+  modules/dotfiles/.zshrc \
+  modules/dotfiles/.commitlintrc \
+  modules/dotfiles/.idea \
+  modules/dotfiles/.vscode \
+  .trunk/configs/.* \
+  .
 
-for ITEM in $(echo """
-${DIR}/modules/dotfiles/.devcontainer
-${DIR}/modules/dotfiles/.gitignore
-${DIR}/modules/dotfiles/.ssh
-${DIR}/modules/dotfiles/.trunk
-${DIR}/modules/dotfiles/compose-dev.yaml
-"""); do
-  # copy if not present in "${DIR}"
-  ls -l $(basename "${ITEM}") > /dev/null 2>&1 || cp -r "${ITEM}" "${DIR}/"
+ITEMS=(
+  "modules/dotfiles/.devcontainer"
+  "modules/dotfiles/.editorconfig"
+  "modules/dotfiles/.gitignore"
+  "modules/dotfiles/.ssh"
+  "modules/dotfiles/.trunk"
+  "modules/dotfiles/compose-dev.yaml"
+)
+for ITEM in "${ITEMS[@]}"; do
+  # copy if not present in the root directory
+  ls -l $(basename "${ITEM}") >/dev/null 2>&1 || cp -r "${ITEM}" .
 done
 
 sudo chsh -s "$(command -v zsh)" "${USER}"
 
 # trunk.io
-if [[ -n $(command -v "usermod") ]]; then
+if [[ -n $(command -v "trunk") ]]; then
   trunk fmt
   trunk check
 fi
